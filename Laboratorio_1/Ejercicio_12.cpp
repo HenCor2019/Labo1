cbad2d169c68f512de3908f37986bb040de4f829
HenCor2019 <54414736+HenCor2019@users.noreply.github.com>
2019-09-12 20:20:46 -0600

    Invertir un numero

---
+++ Laboratorio_1/Ejercicio_12.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+//Ejercicio 12
+//Invertir un numero dado por el usuario
+/*
+int Inver(int numero){
+    int residuo=0,NumInv=0;
+    while(numero>0){
+      residuo=numero%10;
+      numero/=10;
+      NumInv=(NumInv*10)+residuo;
+    }
+    return NumInv;
+}
+
+int main(){
+    int numero=0;
+     cout<<"Dame un numero: "; cin>>numero;
+     cout<<"Tu numero "<<numero<<" , su invertido es: "<<Inver(numero)<<endl;
+    
+ return 0;
+}
+*/
