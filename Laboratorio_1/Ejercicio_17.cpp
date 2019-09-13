fde83e06cc741eb31b55c5ee0d4907fb2367ac18
HenCor2019 <54414736+HenCor2019@users.noreply.github.com>
2019-09-12 20:18:58 -0600

    Suma de 10 enteros

---
+++ Laboratorio_1/Ejercicio_17.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+/*
+//Ejercicio 17 Suma de 10 enteros, dados por el usuario
+
+void suma(int numeros[]){
+ int i=0, total=0;
+ float promedio=0;
+   
+ while(i!=10){
+   cout<<"Dame el valor numero "<<i+1<<": "; cin>>numeros[i];
+   total+=numeros[i];
+   i++;
+ }
+  promedio=total*0.10;
+  
+  cout<<"La suma de tus arreglos es: "<<total<<"\n y el promedio es: "<<promedio;
+}
+
+int main(){
+ 
+ int numeros[10];
+  
+ suma(numeros);
+
+
+ return 0;
+
+}
+*/
