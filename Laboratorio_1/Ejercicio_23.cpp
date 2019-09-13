cf3d1fa7e3cd78284f80361b149f946c6ab177db
HenCor2019 <54414736+HenCor2019@users.noreply.github.com>
2019-09-12 20:22:11 -0600

    Suma de dos complejos

---
+++ Laboratorio_1/Ejercicio_23.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+//Ejercicio 23, suma de complejos
+
+/*
+struct complx{
+     int parteR,parteI,resultadoR,resultadoI;
+}; 
+
+typedef struct complx complejos;
+
+void sumaComplx(struct complx *Pcomplx, struct complx *Scomplx, struct complx *Result){
+      
+  cout<<"Dame la parte real del primer complejo: "; cin>>(*Pcomplx).parteR;
+  cout<<"Dame la parte imaginaria del primer complejo: "; cin>>(*Pcomplx).parteI;
+  cout<<"Dame la parte real del segundo complejo: "; cin>>(*Scomplx).parteR;
+  cout<<"Dame la parte real del segundo complejo: "; cin>>(*Scomplx).parteI;
+  
+  (*Result).resultadoR=(*Pcomplx).parteR + (*Scomplx).parteR;
+  (*Result).resultadoI=(*Pcomplx).parteI  +(*Scomplx).parteI;
+
+// Ya que el signo + lo obvia, hay que ponerlo, pero si el imaginario es negativo no 
+  if((*Result).resultadoI>0)
+  cout<<"La suma de tus complejos es: "<<(*Result).resultadoR<<" + "<<(*Result).resultadoI<<"j";
+  else  
+  cout<<"La suma de tus complejos es: "<<(*Result).resultadoR<<" "<<(*Result).resultadoI<<"j";
+    
+}
+
+
+int main(){
+    complejos Pcomplx,Scomplx,Result;
+    
+    sumaComplx(&Pcomplx,&Scomplx,&Result);
+    
+    
+  return 0;    
+}
+*/
